dcf372a52aea288c2797e0d55fc5656ad75aa411
doanntuann <tuanngodoan@hotmail.com>
2020-04-25 22:59:46 +0700

    Upgrade account UI

---
+++ Sources/Modules/UpgradeAccount/UpgradeAccountViewController.swift
@@ -0,0 +1,24 @@
+//
+//  UpgradeAccountViewController.swift
+//  CS-Living
+//
+//  Created by doanntuann on 4/25/20.
+//
+
+import UIKit
+
+class UpgradeAccountViewController: UIViewController {
+
+    @IBoutlet weak var upgradeAccountButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func upgradeAccountDidTouch(_ sender: UIButton) {
+        
+    }
+
+}
